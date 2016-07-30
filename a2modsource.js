ff3d77aff0bd270f11b26b39e296629607cbce19
ewerybody <ewerybody@braincontrol.org>
2016-07-30 19:14:15 +0200

    very first a2.modules test checkin

---
+++ a2modsource.js
@@ -0,0 +1,8 @@
+﻿{
+    "name": "a2.modules",
+    "maintainer": "ewerybody",
+    "maintainer_at": "gmail.com",
+    "url": "",
+    "version": "0.1",
+    "description": "The standard a2 script module collection."
+}
