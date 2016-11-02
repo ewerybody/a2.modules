be141bf381fb2feb12fdb3d57418442790e25c22
ewerybody <ewerybody+github@gmail.com>
2016-11-02 01:02:01 +0100

    wip #4

--- HotStrings/hotstrings.py
+++ HotStrings/hotstrings.py
@@ -6,16 +6,97 @@ Some element description ...
 """
 import os
 import a2ctrl
-from PySide import QtGui
+from PySide import QtGui, QtCore
 from a2element import DrawCtrl, EditCtrl
 from a2widget.a2item_editor import A2ItemEditor
+from a2widget.textfield_autoheight import TextField_AutoHeight
+from collections import OrderedDict
+from pprint import pprint
+
+
+hs_checkboxes = [
+    # key, label
+    ('instant', 'Triggered Immediately (otherwise by Space, Enter ...)'),
+    ('ignore', 'Ignore Characters Causing Replacement'),
+    ('inside', 'Replace Inside Words'),
+    ('append', 'Don\'t Replace Abbreviation, Just append'),
+    ('raw', 'Output Control-Characters as Plain Text (like {Enter})'),
+    ('substitute', 'substitute !, +, ^, and # with Alt, Shift, Ctrl or Windows'),
+    ('cmdmode', 'Autohotkey Command Mode'),
+    ('sendplay', 'SendPlay Mode')]
+default_dict = dict([(name, False) for name, _ in hs_checkboxes])
+default_dict.update({'case': 0, 'scope': 0, 'text': '', 'scope_field': ''})
 
 
 class HotStringsEditor(A2ItemEditor):
-    def __init__(self, parent):
+    _cfg_changed = QtCore.Signal(str)
+    hotstring_changed = QtCore.Signal()
+
+    def __init__(self, user_cfg, parent):
         super(HotStringsEditor, self).__init__(parent)
-        self.config_layout = QtGui.QVBoxLayout(self)
-        self.ui.item_editor_layout.addLayout(self.config_layout)
+        self.user_cfg = user_cfg
+        print('self.user_cfg:')
+        pprint(self.user_cfg)
+        self.fill_items(self.user_cfg.get('value', {}).keys())
+        self._current_cfg = {}
+        self._config_widgets = OrderedDict()
+
+        self.ui.text = TextField_AutoHeight(self)
+        self.ui.text.setObjectName('text')
+        self._config_widgets['text'] = self.ui.text
+
+        for name, label in hs_checkboxes:
+            checkbox = QtGui.QCheckBox(self)
+            checkbox.setText(label)
+            checkbox.setObjectName(name)
+            self._config_widgets[name] = checkbox
+
+        self.ui.case = QtGui.QComboBox(self)
+        self.ui.case.setObjectName('case')
+        self.ui.case.addItems(['Ignore Case', 'Case Sensitive', 'Don\'t Conform To Typed Case'])
+        self._config_widgets['case'] = self.ui.case
+
+        self.ui.scope = QtGui.QComboBox(self)
+        self.ui.scope.setObjectName('scope')
+        self.ui.scope.addItems(['Scope: Global', 'Scope: Only In:', 'Scope: Not In:'])
+        self._config_widgets['scope'] = self.ui.scope
+
+        self.ui.scope_field = QtGui.QLineEdit(self)
+        self.ui.scope_field.setObjectName('scope_field')
+        self._config_widgets['scope_field'] = self.ui.scope_field
+
+        for widget in self._config_widgets.values():
+            self.ui.config_layout.addWidget(widget)
+
+        a2ctrl.connect.control_list(self._config_widgets.values(), self._current_cfg, self._cfg_changed)
+
+        spacer = QtGui.QSpacerItem(0, 0, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.ui.config_layout.addItem(spacer)
+
+        self._cfg_changed.connect(self.update_config)
+
+        self.selected_text_changed.connect(self.fill_data)
+
+    def update_config(self):
+        diff_dict = {}
+        for key, value in self._current_cfg.items():
+            if value != default_dict[key]:
+                diff_dict[key] = value
+        if diff_dict:
+            self.user_cfg[self.selected_text] = diff_dict
+        elif self.selected_text in self.user_cfg:
+            del self.user_cfg[self.selected_text]
+        self.hotstring_changed.emit()
+
+    def fill_data(self, item):
+        print('item: %s' % item)
+        print('item in self.user_cfg: %s' % (item in self.user_cfg))
+        cfg = self.user_cfg.get(item, default_dict)
+
+        for name, widget in self._config_widgets.items():
+            value = cfg.get(name, default_dict[name])
+            if isinstance(default_dict[name], bool):
+                widget.setChecked(value)
 
 
 class Draw(DrawCtrl):
@@ -24,6 +105,7 @@ class Draw(DrawCtrl):
     to change the default behavior of the element.
     """
     def __init__(self, main, cfg, mod):
+        cfg.setdefault('name', 'user_hotstrings')
         super(Draw, self).__init__(main, cfg, mod)
         self._setupUi()
 
@@ -32,9 +114,16 @@ class Draw(DrawCtrl):
 
     def _setupUi(self):
         self.layout = QtGui.QVBoxLayout(self)
-        self.editor = HotStringsEditor(self)
+        self.editor = HotStringsEditor(self.userCfg or {}, self)
+        self.editor.hotstring_changed.connect(self.delayed_check)
         self.layout.addWidget(self.editor)
         self.setLayout(self.layout)
+
+    def check(self, *args):
+        DrawCtrl.check(self, *args)
+        print('self.editor.user_cfg: %s' % self.editor.user_cfg)
+        self.set_user_value(self.editor.user_cfg.get('value', {}))
+        self.change()
 
 
 class Edit(EditCtrl):
