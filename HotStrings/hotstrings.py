06775a1a3922f14f87a7751a4e992e57d98ec36b
ewerybody <ewerybody+github@gmail.com>
2016-11-09 16:53:48 +0100

    Removed dbg printing. Making get_settings of mod MUCH slimmer!

--- HotStrings/hotstrings.py
+++ HotStrings/hotstrings.py
@@ -198,31 +198,6 @@ class Edit(EditCtrl):
 
 def get_settings(module_key, cfg, db_dict, user_cfg):
     """
-    Called by the module on "change" to get an elements data thats
-    eventually written into the runtime includes.
-
-    Passed into is all you might need:
-    :param str module_key: "module_source_name|module_name" combo used to identify module in db
-    :param dict cfg: Standard element configuration dictionary.
-    :param dict db_dict: Dictionary that's used to write the include data with "hotkeys", "variables" and "includes" keys
-    :param dict user_cfg: This elements user edits saved in the db
-
-    To make changes to the:
-    * "variables" - a simple key, value dictionary in db_dict
-
-    Get the current value via get_cfg_value() given the default cfg and user_cfg.
-    If value name is found it takes the value from there, otherwise from cfg or given default.
-
-        value = a2ctrl.get_cfg_value(cfg, user_cfg, typ=bool, default=False)
-
-    write the key and value to the "variables" dict:
-
-        db_dict['variables'][cfg['name']] = value
-
-    * "hotkeys" - a dictionary with scope identifiers
-
-    * "includes" - a simple list with ahk script paths
+    So far this only adds the hotstrings.ahk to includes.
     """
-    stuff = a2ctrl.get_cfg_value(cfg, user_cfg, typ=dict, default={})
-    print('get_settings stuff: %s' % pprint.pformat(stuff))
     db_dict.setdefault('settings_includes', []).append(HOTSTRINGS_FILENAME)
