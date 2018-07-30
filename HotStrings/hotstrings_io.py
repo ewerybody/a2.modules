bd497a37b5b4d0f9ea21c3ecc0d4456a9fdcc968
Eric Werner <ewerybody+github@gmail.com>
2018-07-30 20:45:01 +0200

    more on #5 importing almost done

--- HotStrings/hotstrings_io.py
+++ HotStrings/hotstrings_io.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import codecs
 import enum
+import os
 
 
 # for the on/off options
@@ -52,7 +53,7 @@ def dict_to_hotstrings(hotstrings_data):
         elif mode == 3:
             options += 'R'
         elif mode == 4:
-            options += 't'
+            options += 'T'
 
         lines.append(f'{options}:{hotstring}::{text}')
 
@@ -61,7 +62,7 @@ def dict_to_hotstrings(hotstrings_data):
 
 
 def hotstrings_file_to_dict(path):
-    hotstrings = {}
+    hs_dict = {}
     current_scope = '#IfWinActive,'
     with codecs.open(path, encoding='utf-8-sig') as fobj:
         for line in fobj:
@@ -69,11 +70,52 @@ def hotstrings_file_to_dict(path):
             if line.lower().startswith('#ifwin'):
                 current_scope = line
             if line.startswith(':'):
-                hotstrings[current_scope] = line
-                print('line:', line)
+                if not '::' in line or not ':' in line[1:]:
+                    print('Not a hotstring? "%s"' % line)
+                    continue
+
+                this_hs = {}
+                options, rest = line[1:].split(':', 1)
+
+                # if the shurtcut does not start with :: its easy
+                if not rest.startswith('::'):
+                    shortcut, text = rest.split('::', 1)
+                # otherwise we need to search for the first non-":"
+                else:
+                    for i, l in enumerate(rest):
+                        if l != ':':
+                            break
+                    pos = rest[i:].find('::')
+                    if pos == -1:
+                        print('Not a hotstring? "%s"' % line)
+                        continue
+                    shortcut = rest[:pos + i]
+                    text = rest[pos + i + 2:]
+                this_hs['text'] = text
+
+                # disassemble the options
+                for op in Options:
+                    if op.value in options:
+                        this_hs[op.name] = True
+                for name, option_list in OPTION_LISTS.items():
+                    for i, op in enumerate(option_list):
+                        # we do not break because found "C" can still be "C1"
+                        if op in options:
+                            this_hs[name] = i + 1
+                for op, mode_index in [('x', 1,), ('r', 3), ('t', 4)]:
+                    if op in options:
+                        this_hs['mode'] = mode_index
+
+                hs_dict.setdefault(current_scope, {})[shortcut] = this_hs
+    return hs_dict
 
 
 if __name__ == '__main__':
-    # p = 'hotstrings.ahk'
-    # hotstrings_file_to_dict(p)
-    pass
+    this_dir = os.path.dirname(__file__)
+    test_file = os.path.join(this_dir, 'test', 'teststrings.ahk')
+    from pprint import pprint
+    hs_dict = hotstrings_file_to_dict(test_file)
+    pprint(hs_dict)
+
+    hs_code = dict_to_hotstrings(hs_dict['#IfWinActive,'])
+    print('hs_code:\n%s' % hs_code)
