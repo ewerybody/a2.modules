028caf3d5dac19228ac376a61f582c3b70f9b60c
Eric Werner <ewerybody+github@gmail.com>
2018-11-20 23:53:16 +0100

    yay! menu item code to python script files

---
+++ HotStrings/a2_menu_item_export_hotstring_json.py
@@ -0,0 +1,8 @@
+﻿# a2 menu item script "export_hotstring_json"
+
+def main(main, mod):
+    """
+    :param main: Main window instance.
+    :param mod: Current a2 module instance.
+    """
+    print('Export Hotstrings JSON ...')
