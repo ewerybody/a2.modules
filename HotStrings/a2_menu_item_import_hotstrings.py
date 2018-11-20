0ac1ab10e4fb9fb6c764d30719ee03d85d76b9f8
Eric Werner <ewerybody+github@gmail.com>
2018-11-20 23:53:16 +0100

    yay! menu item code to python script files

---
+++ HotStrings/a2_menu_item_import_hotstrings.py
@@ -0,0 +1,15 @@
+﻿# a2 menu item script "import_hotstring"
+
+def main(main, mod):
+    print('Import Hotstrings ... %s' % __name__)
+
+    from PySide2 import QtWidgets
+
+    file_path, _ = QtWidgets.QFileDialog.getOpenFileName(
+        None, 'Import Hotstrings Data', mod.path, '(*.ahk *.json)')
+
+    if file_path:
+        import os
+        import a2util
+        cwd = os.getcwd()
+        print(cwd)
