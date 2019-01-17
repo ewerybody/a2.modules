e4947df3242b3839457d24427671a05cd955f1e1
Eric Werner <ewerybody+github@gmail.com>
2019-01-17 14:46:29 +0100

    finished #5 Hotstrings can finally be imported and exported 👍

--- HotStrings/a2_menu_item_export_hotstring_ahk_code.py
+++ HotStrings/a2_menu_item_export_hotstring_ahk_code.py
@@ -1,8 +1,22 @@
 ﻿# a2 menu item script "export_hotstring_ahk_code"
+from PySide2 import QtWidgets
+
 
 def main(a2, mod):
     """
     :param a2: Main A2 object instance.
     :param mod: Current a2 module instance.
     """
-    print('Export Hotstrings Code ...')
+    title = 'Export Hotstrings Code ...'
+    print(title)
+
+    file_path, _filter = QtWidgets.QFileDialog.getSaveFileName(
+        None, title, a2.paths.a2, '*.ahk')
+
+    if file_path:
+        import a2util
+        import hotstrings_io
+        name = 'hotstrings'
+        hs_current = mod.get_user_cfg()[name]
+        code = hotstrings_io.dict_to_ahkcode(hs_current)
+        a2util.write_utf8(file_path, code)
--- HotStrings/a2_menu_item_export_hotstring_json.py
+++ HotStrings/a2_menu_item_export_hotstring_json.py
@@ -1,8 +1,20 @@
 ﻿# a2 menu item script "export_hotstring_json"
+from PySide2 import QtWidgets
+
 
 def main(a2, mod):
     """
     :param a2: Main A2 object instance.
     :param mod: Current a2 module instance.
     """
-    print('Export Hotstrings JSON ...')
+    title = 'Export Hotstrings JSON ...'
+    print(title)
+
+    file_path, _filter = QtWidgets.QFileDialog.getSaveFileName(
+        None, title, a2.paths.a2, '*.json')
+
+    if file_path:
+        import a2util
+        name = 'hotstrings'
+        hs_current = mod.get_user_cfg()[name]
+        a2util.json_write(file_path, hs_current)
