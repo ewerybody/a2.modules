adc53ad6708bad3aba42e76d84946f025edd89bb
Eric Werner <ewerybody+github@gmail.com>
2020-08-17 20:52:13 +0200

    gtranslate: Language lister connected with configs

--- gtranslate/a2_local_element_gtranslate_lister.py
+++ gtranslate/a2_local_element_gtranslate_lister.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 from PySide2 import QtWidgets, QtCore
 
 import a2ctrl
-import a2util
+import a2widget.a2hotkey
 import a2element.hotkey
 from a2element import DrawCtrl, EditCtrl
 from a2widget import a2item_editor, a2input_dialog, a2hotkey
@@ -39,59 +39,80 @@ _DEFAULT_HOTKEY = {
 class GTranslateLister(a2item_editor.A2ItemEditor):
     def __init__(self, cfg, parent):
         super(GTranslateLister, self).__init__(parent)
+        self.item_flags = (
+            QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsEnabled)
+        self.set_data(cfg)
 
         self._hk_user_cfg = {}
         hk_config = deepcopy(_DEFAULT_HOTKEY)
         self._dummymod = _ModStub(hk_config)
         self.hotkey = a2element.hotkey.Draw(self, hk_config, self._dummymod, self._hk_user_cfg)
         self.add_row(self.hotkey)
-        # self.add_data_widget('hotkey', hotkey, hotkey.set_key)
-        # ignore_check = QtWidgets.QCheckBox(self)
-        # ignore_check.setText('Ignore this one')
-        # self.add_data_widget('ignore', ignore_check, ignore_check.setChecked, default_value=False)
         self.selected_name_changed.connect(self._update_hotkey)
 
         self.hotkey.hotkey_button.hotkey_changed.connect(self._changed)
         self.hotkey.hotkey_button.scope_changed.connect(self._changed)
         self.hotkey.checkbox.clicked.connect(self._changed)
+        self.ui.list_layout_widget.setMaximumWidth(150)
+
+        if cfg:
+            self.ui.item_list.select_names([sorted(cfg.keys())[0]])
 
     def add_item(self):
-        dialog = NewDialog(self)
+        dialog = NewDialog(self, self.data.keys())
         dialog.okayed.connect(self._add)
         dialog.show()
 
     def _add(self):
         key = self.sender().output
         self.add_named_item(key)
+        if key not in self.data:
+            self._changed()
 
     def _update_hotkey(self, name):
-        from_lang, to_lang = name.split(gtranslate_langs.SEPARATOR)
+        try:
+            from_lang, to_lang = name.split(gtranslate_langs.SEPARATOR)
+        except ValueError:
+            return
+
         text = 'Translate "%s" to "%s"' % (gtranslate_langs.key_to_name(from_lang),
                                            gtranslate_langs.key_to_name(to_lang))
         self.hotkey.label.setText(text)
-        this_cfg = self.data(name, {})
+        this_cfg = self.data.get(name, {})
+        self.hotkey.checkbox.blockSignals(True)
+        self.hotkey.hotkey_button.blockSignals(True)
         self.hotkey.hotkey_button.set_config(this_cfg)
+        self.hotkey.checkbox.setChecked(this_cfg.get('enabled', True))
+        self.hotkey.checkbox.blockSignals(False)
+        self.hotkey.hotkey_button.blockSignals(False)
 
     def _changed(self):
-        self
+        self.data[self.selected_name] = self.hotkey.get_user_dict()
+        self.data_changed.emit()
 
 
 class _ModStub:
     """A fake module to be able to receive element changes."""
     def __init__(self, config):
         self.config = config
+        self.enabled = False
 
     def set_user_cfg(self, cfg, this, name):
         cfg[name] = this
 
+    def change(self):
+        pass
+
 
 class NewDialog(a2input_dialog.A2ConfirmDialog):
+    """Dialog to select the languages to translate between."""
     okayed = QtCore.Signal(str)
     field_changed = QtCore.Signal(str)
 
-    def __init__(self, parent):
+    def __init__(self, parent, present_keys):
         super(NewDialog, self).__init__(
             parent, 'New gtranslate Hotkey', 'Select languages to translate between:')
+        self._present_keys = present_keys
 
         self.ui.combo_from = QtWidgets.QComboBox(self)
         self.ui.combo_from.addItem(gtranslate_langs.AUTO_LANGUAGE)
@@ -104,11 +125,15 @@ class NewDialog(a2input_dialog.A2ConfirmDialog):
             self.ui.combo_to.addItem(f'{name} ({key})')
             self.ui.combo_to.setItemData(i, key)
 
+        if gtranslate_langs.DEFAULT_TRANSLATION not in self._present_keys:
+            name = gtranslate_langs.key_to_name(gtranslate_langs.DEFAULT)
+            self.ui.combo_to.setCurrentText(f'{name} ({gtranslate_langs.DEFAULT})')
+
         self.ui.combo_from.currentIndexChanged.connect(self.check)
         self.ui.combo_to.currentIndexChanged.connect(self.check)
-        self.ui.combo_from.setFocus()
+        self.ui.combo_to.setFocus()
 
-        form = QtWidgets.QFormLayout(self)
+        form = QtWidgets.QFormLayout()
         form.addRow('From:', self.ui.combo_from)
         form.addRow('To:', self.ui.combo_to)
         self.ui.main_layout.insertLayout(1, form)
@@ -157,11 +182,16 @@ class Draw(DrawCtrl):
         self.main_layout = QtWidgets.QVBoxLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.editor = GTranslateLister({}, self)
+        self.editor = GTranslateLister(self.user_cfg, self)
         self.editor.data_changed.connect(self.delayed_check)
         self.main_layout.addWidget(self.editor)
 
         self.is_expandable_widget = True
+
+    def check(self):
+        self.user_cfg = self.editor.data
+        self.set_user_value(self.user_cfg)
+        self.change()
 
 
 class Edit(EditCtrl):
@@ -183,30 +213,29 @@ class Edit(EditCtrl):
 
 
 def get_settings(module_key, cfg, db_dict, user_cfg):
+    """FIXME: This is currently quite a copy of the hotkey element get_settings.
+    In future this needs to be some sort of make_hotkey-function...
+    Not stuffing data into a dict like this :/.
     """
-    Called by the module on "change" to get an elements data thats
-    eventually written into the runtime includes.
+    for translation_name, translation_cfg in user_cfg.items():
+        Vars = a2widget.a2hotkey.Vars
+        if not translation_cfg.get('enabled', False):
+            continue
 
-    Passed into is all you might need:
-    :param str module_key: "module_source_name|module_name" combo used to identify module in db
-    :param dict cfg: Standard element configuration dictionary.
-    :param dict db_dict: Dictionary that's used to write the include data with "hotkeys", "variables" and "includes" keys
-    :param dict user_cfg: This elements user edits saved in the db
+        key = a2ctrl.get_cfg_value(cfg, translation_cfg, 'key')
+        scope = a2ctrl.get_cfg_value(cfg, translation_cfg, Vars.scope, list)
+        scope_mode = a2ctrl.get_cfg_value(cfg, translation_cfg, Vars.scope_mode, int)
+        try:
+            from_lang, to_lang = translation_name.split(gtranslate_langs.SEPARATOR)
+        except ValueError:
+            continue
+        func = f'gtranslate("{from_lang}", "{to_lang}")'
 
-    To make changes to the:
-    * "variables" - a simple key, value dictionary in db_dict
+        db_dict.setdefault('hotkeys', {})
+        db_dict['hotkeys'].setdefault(scope_mode, [])
+        # save a global if global scope set or all-but AND scope is empty
+        if scope_mode == 0 or scope_mode == 2 and scope == '':
+            db_dict['hotkeys'][0].append([key, func])
+        else:
+            db_dict['hotkeys'][scope_mode].append([scope, key, func])
 
-    Get the current value via get_cfg_value() given the default cfg and user_cfg.
-    If value name is found it takes the value from there, otherwise from cfg or given default.
-
-        value = a2ctrl.get_cfg_value(cfg, user_cfg, typ=bool, default=False)
-
-    write the key and value to the "variables" dict:
-
-        db_dict['variables'][cfg['name']] = value
-
-    * "hotkeys" - a dictionary with scope identifiers
-
-    * "includes" - a simple list with ahk script paths
-    """
-    pass
